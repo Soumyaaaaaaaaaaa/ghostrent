6a0f2bf65aba85892687f79a5014274ba1a114bd
soumya pandey <soumyapandey791@gmail.com>
2026-04-01 21:19:48 +0530

    Add files via upload

---
+++ contact.jsx
@@ -0,0 +1,11 @@
+function Contact() {
+  return (
+    <div style={{ textAlign: "center", padding: "40px" }}>
+      <h1>Contact</h1>
+      <p>Email: support@rentshield.com</p>
+      <p>Phone: 9876543210</p>
+    </div>
+  );
+}
+
+export default Contact;
